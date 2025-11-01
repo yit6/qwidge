4626dceb191d4290ec5391da1afa09cfbd71fe4e
Int3gerunderflow <canidiansnowman@gmail.com>
2025-11-01 16:54:00 -0400

    Setup minimal working RESTapi

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,16 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+var cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
 dotenv.config()
+const port = 8080
 
 
 // view engine setup
