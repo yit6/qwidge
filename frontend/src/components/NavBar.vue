eca15c583e532f20f206dac9d4fe0ee45dc6ff2c
sowgro <tpoke.ferrari@gmail.com>
2025-11-01 19:01:11 -0400

    Stub out mvp

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<script setup>
+  
+</script>
+
+<template>
+  <ul>
+    <li><router-link to="Landing.vue">Home</router-link></li>
+    <li><router-link to="ServiceList.vue">Services</router-link></li>
+  </ul>
+</template>
+
+<style scoped>
+
+</style>
