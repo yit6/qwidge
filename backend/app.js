770954150b2ac32bb3b02749a3e8bfb2cac6363b
yit <thatsjustwyatt@gmail.com>
2025-11-01 17:00:40 -0400

    remove http-errors

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,3 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
@@ -22,22 +21,6 @@ app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
